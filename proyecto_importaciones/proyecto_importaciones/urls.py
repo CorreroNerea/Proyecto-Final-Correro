3dc21464f0fac1c9fea48e51bc45916a12af7c36
Nerea Correro <162913707+CorreroNerea@users.noreply.github.com>
2024-08-27 18:29:30 -0300

    Update urls.py

--- proyecto_importaciones/proyecto_importaciones/urls.py
+++ proyecto_importaciones/proyecto_importaciones/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppImportaciones/', include('AppImportaciones.urls') ),
+    path('', include('AppImportaciones.urls') ),
 ]
 
